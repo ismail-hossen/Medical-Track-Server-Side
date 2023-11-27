914c5e5c5233d0a413611594f9c4ab1270287d46
ismail-hossen <satict2016@gmail.com>
2023-11-27 11:38:30 +0600

    created api for update and delete a camp

--- src/controllers/medicalCamp/camp.controller.js
+++ src/controllers/medicalCamp/camp.controller.js
@@ -11,4 +11,36 @@ const createCamp = async (req, res, next) => {
   }
 };
 
-module.exports = { createCamp };
+const campsByOrganizer = async (req, res, next) => {
+  try {
+    const result = await CampModel.find({
+      "author.email": req.params.email,
+    });
+    res.status(200).send(result);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deleteCamp = async (req, res, next) => {
+  try {
+    const result = await CampModel.deleteOne({ _id: req.params.campId });
+    res.status(200).send(result);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateCamp = async (req, res, next) => {
+  try {
+    const result = await CampModel.updateOne(
+      { _id: req.params.campId },
+      { $set: req.body }
+    );
+    res.status(200).send(result);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { createCamp, campsByOrganizer, deleteCamp, updateCamp };
--- src/routes/camp/camp.routes.js
+++ src/routes/camp/camp.routes.js
@@ -1,7 +1,15 @@
-const { createCamp } = require("../../controllers/medicalCamp/camp.controller");
+const {
+  createCamp,
+  campsByOrganizer,
+  deleteCamp,
+  updateCamp,
+} = require("../../controllers/medicalCamp/camp.controller");
 
 const campRouter = require("express").Router();
 
+campRouter.get("/camps-by-organizer/:email", campsByOrganizer);
 campRouter.post("/create-camp", createCamp);
+campRouter.patch("/update-camp/:campId", updateCamp);
+campRouter.delete("/delete-camp/:campId", deleteCamp);
 
 module.exports = campRouter;
